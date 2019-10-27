1f74ed6cf40ea3ad34b7e033672132733cad762b
Ilya <iliyazelenkog@gmail.com>
2019-10-27 22:56:28 +0200

    improvement: update favicon and icon. New title.

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -14,8 +14,8 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s - Keyboard speed trainer',
+    title: 'Keyboard speed trainer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
