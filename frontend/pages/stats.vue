b65b0fa9b41c28650a06e1d6dd24a2fabeffb0fa
Ilya Zelen <iliyazelenkog@gmail.com>
2023-01-05 10:40:42 +0200

    improvement(stats): more stats, fix change lang

--- frontend/pages/stats.vue
+++ frontend/pages/stats.vue
@@ -28,9 +28,19 @@
               v-model="currentLang"
               class="ml-auto"
               with-any-option
+              @input="rebuild"
             />
           </div>
         </div>
+
+        <template v-if="averageLast15Results != null">
+          <br>
+          Average score over 15 attempts: <b>{{ averageLast15Results.toFixed(2) }}</b>
+        </template>
+        <template v-if="totalAttempts != null">
+          <br>
+          Total attempts: <b>{{ totalAttempts }}</b>
+        </template>
 
         <VContainer>
           <canvas id="chart" />
@@ -56,7 +66,11 @@ export default {
     return {
       currentLang: this.$cookies.get('currentLang') || 'en',
       user: null,
-      loadingUser: true
+      loadingUser: true,
+      chart1: null,
+      chart2: null,
+      totalAttempts: null,
+      averageLast15Results: null
     }
   },
   async mounted () {
@@ -70,6 +84,13 @@ export default {
     })
   },
   methods: {
+    rebuild () {
+      this.chart1.destroy()
+      this.chart2.destroy()
+
+      this.initChart()
+      // this.$forceUpdate()
+    },
     async initChart () {
       const q = query(
         resultsCollection,
@@ -83,12 +104,10 @@ export default {
       const toDate = date => date.toDate().toLocaleDateString()
       const labels = results.map(i => toDate(i.createdAt))
 
-      const countByDate = results.reduce((groups, i) => {
-        const key = toDate(i.createdAt)
-        const count = (groups[key] || 0)
-        groups[key] = count + 1
-        return groups
-      }, {})
+      this.totalAttempts = results.length
+      this.averageLast15Results = results
+        .slice(-15)
+        .reduce((sum, i) => sum + i.CPM, 0) / 15
 
       const data = {
         labels,
@@ -96,16 +115,17 @@ export default {
           {
             label: 'WPM (Words Per Minute)',
             data: results.map(i => i.WPM),
-            fill: true
+            fill: true,
+            tension: 0.9
           },
           {
             label: 'CPM (Characters Per Minute)',
-            data: results.map(i => i.CPM)
+            data: results.map(i => i.CPM),
+            tension: 0.6
           }
         ]
       }
-      // eslint-disable-next-line no-unused-vars
-      const chart = new Chart(
+      this.chart1 = new Chart(
         document.getElementById('chart'),
         {
           type: 'line',
@@ -135,17 +155,34 @@ export default {
           }
         }
       )
-      console.log(Object.values(countByDate))
-      // eslint-disable-next-line no-new
-      new Chart(
+
+      const countByDate = results.reduce((groups, i) => {
+        const key = toDate(i.createdAt)
+        groups[key] ??= {
+          count: null,
+          sumWPM: null
+        }
+        const count = (groups[key].count ?? 0)
+        groups[key].count = count + 1
+        groups[key].sumWPM += i.WPM
+
+        return groups
+      }, {})
+
+      const values = Object.values(countByDate)
+      this.chart2 = new Chart(
         document.getElementById('chart-count'),
         {
-          type: 'bar',
           data: {
             labels: Object.keys(countByDate),
             datasets: [{
+              type: 'bar',
               label: 'Number of attempts per day',
-              data: Object.values(countByDate)
+              data: values.map(i => i.count)
+            }, {
+              type: 'line',
+              label: 'Average WPM',
+              data: values.map(i => i.sumWPM / i.count)
             }]
           },
           options: {
